5ebb72ac1eb6de20ae54af04f217cd5f621021f0
jiyeshin <krjiyeah@outlook.kr>
2018-11-19 12:00:51 +0900

    1119

--- WebData1116/ViewController.swift
+++ WebData1116/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var 이미지뷰: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -23,7 +25,37 @@ class ViewController: UIViewController {
         //print(data)
         
         let result = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-        print(result)
+        //print(result)
+        
+        //파일 매니저 객체를 생성
+        let fm = FileManager.default
+        //도큐먼트 디렉토리 경로를 생성
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let docDir = dirPaths[0]
+        //파일 경로를 생성
+        let filePath = docDir + "/tanghuroo2.jpg"
+        
+        //파일이 없다면
+        if fm.fileExists(atPath: filePath) == false{
+            //이미지 파일의 내용을 다운로드 받기
+            let imgAddr = "http://recipe1.ezmember.co.kr/cache/recipe/2018/04/05/890bd5a6edee83d90cddf312a98c209c1.jpg"
+            var imageUrl = URL(string: imgAddr)
+            let imageData = try! Data(contentsOf: imageUrl!)
+            
+            //다운로드 받은 데이터로 파일을 생성
+            fm.createFile(atPath: filePath, contents: imageData, attributes: nil)
+            print("다운로드")
+        }
+        
+        //파일을 읽기
+        let dataBuffer = fm.contents(atPath: filePath)
+       
+        //이미지 데이터로 변환
+        let img = UIImage(data: dataBuffer!)
+        
+        //화면에 출력
+        이미지뷰.image = img
+        
     }
 }
 
